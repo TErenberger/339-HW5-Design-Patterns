47a02c40c2546f53acc44bb09ec159f114f36620
Trey Erenberger <treye22@gmail.com>
2014-10-30 16:51:40 -0500

    Added functionality to ChangeObserver
    ChangeObserver now reports the amount that the value has changed when
    the subject is updated.

--- ObserverExample/src/ChangeObserver.java
+++ ObserverExample/src/ChangeObserver.java
@@ -1,15 +1,18 @@
 
 public class ChangeObserver extends IntegerObserver {
-
+	
+	int previousValue;
+	
 	public ChangeObserver(VerboseInteger inSubject) {
 		super(inSubject);
-		
+		this.previousValue = inSubject.getValue();
 	}
 
 	@Override
 	public void update() {
-		
-		System.out.println("\nChange Observer has been updated that subject has changed\n");
+		int diff =  subject.getValue() - this.previousValue;
+		System.out.println("\nChange Observer has been updated that subject has changed\nSubject changed by: " + diff + "\n");
+		this.previousValue = subject.getValue();
 
 	}
 
--- ObserverExample/src/ObserverExampleRun.java
+++ ObserverExample/src/ObserverExampleRun.java
@@ -2,14 +2,14 @@
 public class ObserverExampleRun {
 	public static void main(String[] args)
 	{
-		VerboseInteger subject = new VerboseInteger(10);
+		VerboseInteger subject = new VerboseInteger(1000);
 		
 		ChangeObserver booleanObs = new ChangeObserver(subject);
 		ReverseObserver flipobs = new ReverseObserver(subject);
 		
 		subject.setValue(1002);
 		subject.setValue(1993);
-		
+		subject.setValue(1000);
 		
 	}
 }
